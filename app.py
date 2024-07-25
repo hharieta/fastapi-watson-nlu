94c4fd686b6954228db278ce4406b0964f399351
Gatovsky <101161255+hharieta@users.noreply.github.com>
2024-07-25 01:13:29 -0500

    New! Added rate limit for API Calls

--- app.py
+++ app.py
@@ -1,33 +1,42 @@
 from fastapi import FastAPI, HTTPException, Request
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
 from pydantic import BaseModel, constr, ValidationError
 from analyzer import emotion_analyzer
+
+limiter = Limiter(key_func=get_remote_address)
 
 app = FastAPI(
     title="Emotion Analyzer API", 
     description="API call IBM Watson NLP to analyze the emotion of a text", 
     version="2.0.0")
 
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
+
 @app.get("/")
-async def root():
+@limiter.limit("5/minute")
+async def root(request: Request):
     return {"Hello": "World"}
 
 @app.get("/api/v2/status")
-async def read_status():
+async def read_status(request: Request):
     return {"status": "OK"}
-
 
 class TextRequest(BaseModel):
     text: constr(min_length=22, max_length=10000) # type: ignore
     language: constr(min_length=2, max_length=2) # type: ignore
 
-
 @app.post("/api/v2/analyze")
-async def analyze_text(text_request: TextRequest) -> dict:
+@limiter.limit("5/minute")
+async def analyze_text(request: Request, text_request: TextRequest) -> dict:
     try:
         return emotion_analyzer(text_request.text, text_request.language)
     except ValidationError as e:
         raise HTTPException(status_code=422, detail=e.errors())
-
 
 if __name__ == "__main__":
     import uvicorn
